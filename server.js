3122cfff3fb615f8a2ebc840e64c5562d0ad1013
chriscode3 <chrisqcode3@gmail.com>
2019-12-30 17:11:30 -0800

    routes established

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 
 // Connect Database
 connectDB();
+
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
